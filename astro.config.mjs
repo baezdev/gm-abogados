5fcb6f520f12f4dbc27240f29d6a3621b765fe8a
Ángel Baez <baezdev@gmail.com>
2026-03-25 20:38:49 -0600

    Remove custom pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -11,19 +11,11 @@ export default defineConfig({
   integrations: [
     react(),
     sitemap({
-      // Personalización opcional
       changefreq: "weekly",
       priority: 1.0,
       lastmod: new Date(),
-
-      // Incluir secciones con anchors
       customPages: [
         "https://gm-abogados.com.mx/",
-        "https://gm-abogados.com.mx/#do-you-indentify",
-        "https://gm-abogados.com.mx/#services",
-        "https://gm-abogados.com.mx/#faq",
-        "https://gm-abogados.com.mx/#success-stories",
-        "https://gm-abogados.com.mx/#contact",
       ],
     }),
   ],
